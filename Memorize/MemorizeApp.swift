319c811a7cac64aaba8cb85de29b5d27e98ab2a7
Georgi Markov <markov.georgi@gmail.com>
2022-08-09 04:46:54 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Georgi Markov on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
